973558ebfcfa3896058f4959c835051d40b40c57
justin <justintrah8@gmail.com>
2025-04-03 21:20:49 +0200

    ok

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,14 +1,14 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { auth } from "@clerk/nextjs/server";
- 
+
 const f = createUploadthing();
- 
+
 const handleAuth = async () => {
   const { userId } = await auth();
   if (!userId) throw new Error("Unauthorized");
   return { userId };
 };
- 
+
 export const ourFileRouter = {
   postImage: f({ image: { maxFileSize: "4MB" } })
     .middleware(() => handleAuth())
@@ -17,5 +17,5 @@ export const ourFileRouter = {
       return { url: file.url };
     }),
 } satisfies FileRouter;
- 
+
 export type OurFileRouter = typeof ourFileRouter;
